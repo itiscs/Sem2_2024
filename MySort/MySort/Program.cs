a0d67a663fae03bf704bc909f6f8e5f6ffdd36cd
itiscs <moudroff@gmail.com>
2024-03-19 15:33:39 +0300

    MySort

---
+++ MySort/MySort/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace MySort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var gr = new Group();
+            gr.Students.Add(new Student()
+            {
+                Id = 1,
+                Name = "Ivan",
+                Age = 22,
+                Marks = new List<int> { 3, 4, 5, 5, 5 }
+            });
+            gr.Students.Add(new Student()
+            {
+                Id = 2,
+                Name = "Pet",
+                Age = 21,
+                Marks = new List<int> { 5, 5, 5, 5, 5 }
+            }); 
+            gr.Students.Add(new Student()
+            {
+                Id = 3,
+                Name = "Maria",
+                Age = 18,
+                Marks = new List<int> { 3, 4, 3, 3, 3 }
+            }); 
+            gr.Students.Add(new Student()
+            {
+                Id = 4,
+                Name = "Anna",
+                Age = 25,
+                Marks = new List<int> { 3, 4, 4, 4, 5 }
+            }); 
+            gr.Students.Add(new Student()
+            {
+                Id = 5,
+                Name = "Alexander",
+                Age = 20,
+                Marks = new List<int> { 3, 3, 3, 5, 5 }
+            });
+
+
+
+            Console.WriteLine(gr);
+
+            Console.WriteLine("***********************************");
+            gr.Sort((x, y) => x.Age > y.Age);
+
+            Console.WriteLine(gr);
+
+            Console.WriteLine("***********************************");
+
+            gr.Sort((x, y) => x.Name.CompareTo(y.Name) >= 0);
+
+            Console.WriteLine(gr);
+
+            Console.WriteLine("***********************************");
+
+            gr.Sort((x, y) => x.Name.Length > y.Name.Length);
+
+            Console.WriteLine(gr);
+
+            Console.WriteLine("***********************************");
+
+
+
+            gr.Sort((x, y) => x.Id > y.Id);
+
+            Console.WriteLine(gr);
+
+        }
+    }
+}
---
+++ MySort/MySort/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MySort
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<int> Marks { get; set; } = new List<int>();
+
+        public override string ToString()
+        {
+            return $"{Id} {Name} {Age} {Marks.Average()}";
+        }
+    }
+
+
+    public class  Group
+    {
+        public List<Student> Students { get; set; } = new List<Student>();
+
+        public void Sort(Func<Student,Student, bool> func)
+        {
+            int n = Students.Count;
+            bool fl = true;
+            for(int i = 0; i < n; i++) 
+                for(int j = 0; j < n - i - 1; j++)
+                    if ( func(Students[j], Students[j+1]) )
+                    {
+                        var s = Students[j];
+                        Students[j] = Students[j + 1];
+                        Students[j + 1] = s;
+                    }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var student in Students) 
+            {
+                sb.AppendLine(student.ToString());
+            }
+            return sb.ToString();
+        }
+
+    }
+}
