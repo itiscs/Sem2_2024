3ea0766620dde4763973f77d38d17b2e5cac5e29
itiscs <moudroff@gmail.com>
2024-03-29 11:15:45 +0300

    Linq Group

--- LinqApp/LinqApp/Program.cs
+++ LinqApp/LinqApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace LinqApp
+﻿using System.Text.RegularExpressions;
+
+namespace LinqApp
 {
     internal class Program
     {
@@ -12,7 +14,7 @@
                 .OrderByDescending(k => k)
                .Take(3);//.First();
 
-            foreach(int i in lst1) 
+            foreach (int i in lst1)
             {
                 Console.WriteLine(i);
             }
@@ -22,13 +24,106 @@
             "avvvvv", "ccccccca"};
 
             var lst3 = lst2.Where(s => s.Contains('a') || s.Contains('b'))
-                .OrderBy(s => s[s.Length-1])
-                .ThenByDescending(s => s.Length);
+                .OrderBy(s => s[s.Length - 1])
+                .ThenByDescending(s => s.Length)
+                .Select(s => s[0]);
 
             foreach (var i in lst3)
             {
                 Console.WriteLine(i);
             }
+
+            Console.WriteLine("************************");
+
+
+            var gr = new List<Student>();
+            gr.Add(new Student()
+            {
+                Id = 1,
+                GroupNum = 308,
+                Name = "Ivan",
+                Age = 22,
+                Marks = new List<int> { 3, 4, 5, 5, 5 }
+            });
+            gr.Add(new Student()
+            {
+                Id = 2,
+                Name = "Petr",
+                GroupNum = 308,
+                Age = 21,
+                Marks = new List<int> { 5, 5, 5, 5, 5, 5 }
+            });
+            gr.Add(new Student()
+            {
+                Id = 3,
+                Name = "Maria",
+                GroupNum = 308,
+                Age = 18,
+                Marks = new List<int> { 3, 4, 3, 3, 3 }
+            });
+            gr.Add(new Student()
+            {
+                Id = 4,
+                Name = "Anna",
+                GroupNum = 307,
+                Age = 25,
+                Marks = new List<int> { 3, 4, 4, 4, 5 }
+            });
+            gr.Add(new Student()
+            {
+                Id = 5,
+                Name = "Alexander",
+                GroupNum = 307,
+                Age = 20,
+                Marks = new List<int> { 3, 3, 3, 5, 5 }
+            });
+            gr.Add(new Student()
+            {
+                Id = 6,
+                Name = "Serg",
+                GroupNum = 309,
+                Age = 20,
+                Marks = new List<int> { 4, 4, 4, 5, 5 }
+            });
+
+
+            //var newGr = gr.Where(s => s.Marks.Average() > 2)
+            //    .OrderBy(s => s.Age)
+            //    .Select(s => new { s.Name, s.Age })
+            //    .Where(a => a.Age > 20);
+            //    //.Max(a => a.Name);
+
+            //foreach (var st in newGr)
+            //{
+            //    Console.WriteLine(st);
+            //}
+
+
+
+            var newGr = gr.GroupBy(s => s.GroupNum)//.Name.Length)
+                .Select(g => new { g.Key, Count = g.Count(),
+                MaxAge = g.Max(s => s.Age),
+                AllMarks = g.SelectMany(s=>s.Marks)});
+
+
+            foreach(var s in newGr)
+            {
+                Console.WriteLine(s);
+                foreach (var m in s.AllMarks)
+                    Console.Write($" {m}");
+                Console.WriteLine();
+            }
+
+            //foreach(var g in newGr )
+            //{
+            //    Console.WriteLine(g.Key);
+            //    foreach(var st in g)
+            //    {
+            //        Console.WriteLine($"    {st}");
+            //    }
+            //}
+
+
 
 
 
---
+++ LinqApp/LinqApp/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqApp
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public int GroupNum { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<int> Marks { get; set; } = new List<int>();
+
+        public override string ToString()
+        {
+            return $"{Id} {Name} {GroupNum} {Age} {Marks.Average()}";
+        }
+    }
+}
