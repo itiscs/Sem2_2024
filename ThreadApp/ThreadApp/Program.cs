f5eb2c3f6ed296000e3e8039b4516b7661ba3f5a
itiscs <moudroff@gmail.com>
2024-04-19 11:19:47 +0300

    Task Parallel

--- ThreadApp/ThreadApp/Program.cs
+++ ThreadApp/ThreadApp/Program.cs
@@ -3,9 +3,11 @@
     internal class Program
     {
 
-        public static int sum = 0, p = 4;
+        public static int sum = 0, sum_par = 0, p = 8;
         public static int n = 5000000;
         public static int[] arr, sum3;
+
+        static object locker = new object();
         static void Main(string[] args)
         {
 
@@ -18,10 +20,10 @@
 
             for(int i = 0; i< n; i++)
             {
-                arr[i] = r.Next(0,1000);
+                arr[i] = r.Next(0,10);
             }
 
-
+            sum2 = 0;
             DateTime t1 = DateTime.Now;
             for (int i = 0; i < n; i++)
             {
@@ -29,13 +31,29 @@
             }
             Console.WriteLine($"Время последовательно - {(DateTime.Now - t1).TotalMilliseconds}");
 
+            var k = n / p;
+            Console.WriteLine($"k = {k}");
 
 
-            Thread thread1 = new Thread(() => Sum(0));
-            Thread thread2 = new Thread(() => Sum(1));
-            Thread thread3 = new Thread(() => Sum(2));
-            Thread thread4 = new Thread(() => Sum(3));
+            //Thread thread1 = new Thread(() => Sum(0, k));
+            //Thread thread2 = new Thread(() => Sum(k, 2*k));
+            //Thread thread3 = new Thread(() => Sum(2*k, 3*k));
+            //Thread thread4 = new Thread(() => Sum(3*k, n));
 
+
+
+            //List<Task> tasks = new List<Task>();
+
+
+            //for (int i = 0; i < p; i++)
+            //    tasks.Add(new Task(() => Sum(i*k,(i+1)*k) ));
+
+
+           
+            //Task task1 = new Task(() => Sum(0, k));
+            //Task task2 = new Task(() => Sum(k, 2 * k));
+            //Task task3 = new Task(() => Sum(2 * k, 3 * k));
+            //Task task4 = new Task(() => Sum(3 * k, n));
 
             //Thread thread1 = new Thread(Print);
             //Thread thread2 = new Thread(Print);
@@ -45,41 +63,80 @@
 
             t1 = DateTime.Now;
 
-            thread1.Start();
-            thread2.Start();
-            thread3.Start();
-            thread4.Start();
+            //tasks.ForEach(t => t.Start());
 
-            thread1.Join();
-            thread2.Join();
-            thread3.Join();
-            thread4.Join();
-            
-            for (int i = 0; i < p; i++)
-                sum += sum3[i];
+            Parallel.For(0, p, Sum);
+
+
+            //task1.Start();
+            //task2.Start();
+            //task3.Start();
+            //task4.Start();
+
+            //thread1.Start();
+            //thread2.Start();
+            //thread3.Start();
+            //thread4.Start();
+
+            //thread1.Join();
+            //thread2.Join();
+            //thread3.Join();
+            //thread4.Join();
+
+
+            //for (int i = 0; i < p; i++)
+            //    sum += sum3[i];
+
+            Task.WaitAll();
 
             //sum = sum3[0] + sum3[1] + sum3[2] + sum3[3];
             Console.WriteLine($"Время параллельно - {(DateTime.Now - t1).TotalMilliseconds}");
 
 
-            Console.WriteLine($"sum = {sum}   sum2 = {sum2}");
+            Console.WriteLine($"sum = {sum_par}   sum2 = {sum2}");
 
             //Print();
 
 
+
+            Parallel.Invoke(
+                Print,
+                () =>
+                {
+                    Console.WriteLine("Вторая работа начинается");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Вторая работа заканчивается");
+                },
+                () => Square(5000));
+
         }
 
+
+        public static void Square(long k )
+        {
+            Console.WriteLine($"Square = {k*k}");
+
+        }
         
 
         public static void Sum(int id)
         {
-            int i1 = id * (n / p);
-            int i2 = (id+1) * (n / p);
-            Console.WriteLine($"thread {id} i1 = {i1} i2 = {i2}");
+            //Console.WriteLine($"thread {Thread.CurrentThread.ManagedThreadId} i1 = {i1} i2 = {i2}");
 
+            int k = n / p;
+            int i1 = k * id;
+            int i2 = k * (id + 1);
+            
+            int local_sum = 0;
             for (int i = i1; i < i2; i++)
-                sum3[id] += arr[i];
+                local_sum += arr[i];
 
+            Console.WriteLine($"task {Task.CurrentId} i1 = {i1} i2 = {i2} local_sum = {local_sum}");
+
+            lock (locker)
+            {
+                sum_par += local_sum;
+            }
 
 
         }
