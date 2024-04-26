c140718c763a78201e8fc7d32c9723e2a1c717d6
itiscs <moudroff@gmail.com>
2024-04-26 11:17:19 +0300

    Рефлексия

---
+++ ReflectionApp/ReflectionApp/MyClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionApp
+{
+    public class MyClass
+    {
+        private int p1;
+
+        public string p2;
+
+        public int Prop1 { get => p1; set => p1 = value; }
+
+        public MyClass(int par1, string par2)
+        {   
+            p1 = par1;
+            p2 = par2;
+        }
+
+
+        public MyClass():this(100,"Hello")
+        {
+        }
+
+
+        private void PrivMethod()
+        {
+            Console.WriteLine("Private method");
+        }
+
+        public void Method1(int k)
+        {
+            p1 += k;
+        }
+
+        public string Method2()
+        {
+            return $"{p2} + {p1}";
+        }
+
+    }
+}
---
+++ ReflectionApp/ReflectionApp/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Reflection;
+
+namespace ReflectionApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type mathType = typeof(System.Math);
+            
+            Type myType = typeof(MyClass);
+
+            MyClass ob = new MyClass();
+
+            Type myType2 = ob.GetType();
+
+            Console.WriteLine("Fields");
+
+            foreach (var f  in myType.GetFields(BindingFlags.DeclaredOnly
+            | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public))
+            {
+                var str = f.IsPublic ? "pubic" : "priv";
+                Console.WriteLine($"{f.Name}  {f.FieldType} {str}");
+            }
+
+            Console.WriteLine("Properties");
+
+            foreach (var p in myType.GetProperties())
+            {
+                Console.WriteLine($"{p.Name}  {p.GetMethod.ReturnParameter.ParameterType}");
+                Console.WriteLine($"{p.SetMethod.GetParameters()[0].Name}  {p.SetMethod.GetParameters()[0].ParameterType}");
+                
+                Console.WriteLine(p.GetGetMethod().Invoke(ob, null));
+            }
+
+            Console.WriteLine("Constructors");
+
+            foreach (var c in myType.GetConstructors())
+            {
+                Console.WriteLine($"{c.Name} {c.GetParameters().Length}");
+                //p.GetGetMethod.Invoke(ob, null);
+            }
+
+            Console.WriteLine("Methods");
+
+            foreach (var m in myType.GetMethods(BindingFlags.DeclaredOnly
+            | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public))
+            {
+                Console.WriteLine($"{m.Name} {m.GetParameters().Length} {m.ReturnType}");
+                
+                if(m.Name == "Method1")
+                    m.Invoke(ob, new object[] { 20 });
+                if (m.Name == "Method2")
+                    Console.WriteLine( m.Invoke(ob,null));
+
+            }
+
+            Console.WriteLine("********************");
+
+            Assembly asm = Assembly.LoadFrom("LinqApp.dll");
+
+            Console.WriteLine(asm.FullName);
+            // получаем все типы из сборки MyApp.dll
+            Type[] types = asm.GetTypes();
+            foreach (Type t in types)
+            {
+                Console.WriteLine(t.Name);
+            }
+
+        }
+    }
+}
