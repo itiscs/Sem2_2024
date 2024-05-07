bb11a8c6ee09c2e42709ecb51e92387201986407
itiscs <moudroff@gmail.com>
2024-05-07 14:56:25 +0300

    Multi Sort

---
+++ MultiSort/MultiSort/MyArray.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiSort
+{
+    public class MyArray<T> where T : IComparable
+    {
+        public int Length { get; }
+        public T[] Arr { get; set; }
+
+        public T[] Temp { get; set; }
+
+
+        public MyArray(int len)
+        {
+            Length = len;
+            Arr = new T[len];
+            Temp = new T[len];
+        }
+
+        public void Init(Func<T> initElem)
+        {
+            for (int i = 0; i < Length; i++)
+                Arr[i] = initElem();
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < Length; i++)
+                sb.Append($"{Arr[i]} ");
+            return sb.ToString();
+        }
+
+
+        public void Sort(int threadsNum)
+        {
+            Barrier b = new Barrier(threadsNum);
+            Parallel.For(0, threadsNum, i =>
+            {
+                ParSort(i, threadsNum, b);
+            });
+        }
+
+        public bool IsSorted(int k1, int k2)
+        {
+            for (int i = k1; i < k2 - 1; i++)
+                if (Arr[i].CompareTo(Arr[i + 1]) > 0)
+                    return false;
+            return true;
+
+        }
+
+
+        public void ParSort(int threadNum, int threads, Barrier b)
+        {
+            int k = Length / threads;
+            int i1 = k * threadNum;
+            int i2 = k * (threadNum + 1);
+            if (threadNum == threads - 1)
+                i2 = Length;
+
+            System.Array.Sort(Arr, i1, i2 - i1);
+            //Console.WriteLine($"i1 = {i1}  i2 = {i2}  thread = {threadNum} sorted = {IsSorted(i1, i2)} ");
+
+            var iter = 2;
+            while (k < Length)
+            {
+                b.SignalAndWait();
+                if (threadNum % iter == 0)
+                {
+                    int k1 = i1;
+                    int k2 = k1 + k;
+                    int k3 = k2 + k;
+                    if (k3 > Length)
+                        k3 = Length;
+                    MergeArr(k1, k2, k3);
+                    //Console.WriteLine($"{k1} {k2} {k3} --  {threadNum} -- {IsSorted(k1,k3)}");
+                }
+                k *= 2;
+                iter *= 2;
+            }
+
+        }
+
+
+        private void MergeArr(int k1, int k2, int k3)
+        {
+            //int[] mas = new int[k3 - k1];
+            int l = k1;
+            int m = k2;
+            for (int i = k1; i < k3; i++)
+            {
+                if (l >= k2)
+                    Temp[i] = Arr[m++];
+                else if (m >= k3)
+                    Temp[i] = Arr[l++];
+
+                else if (Arr[l].CompareTo(Arr[m]) < 0)
+                    Temp[i] = Arr[l++];
+                else
+                    Temp[i] = Arr[m++];
+
+            }
+
+            for (int i = k1; i < k3; i++)
+            {
+                //Console.Write($"{mas[i]} ");
+                Arr[i] = Temp[i];
+            }
+            //Console.WriteLine();
+        }
+
+
+
+    }
+}
---
+++ MultiSort/MultiSort/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MultiSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 10000;
+            var p = 4;
+
+            Console.WriteLine(n * Math.Log2(n));
+
+            Console.WriteLine((n / p) * Math.Log2(n / p) + Math.Log2(p) * n / 2);
+
+            Random r = new Random();
+            var myArr = new MyArray<int>(n);
+            myArr.Init(() => r.Next());
+
+            var t1 = DateTime.Now;
+            myArr.Sort(1);
+            Console.WriteLine(DateTime.Now - t1);
+
+            Console.WriteLine(myArr.IsSorted(0, myArr.Length));
+
+            myArr.Init(() => r.Next());
+            t1 = DateTime.Now;
+            myArr.Sort(p);
+            Console.WriteLine(DateTime.Now - t1);
+
+            Console.WriteLine(myArr.IsSorted(0, myArr.Length));
+        }
+    }
+}
